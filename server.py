6cb8dc7cbe78108ff39ad206eff1a9ca0d99cf0e
RenziPaolo <paolorenzi00.1@gmail.com>
2024-11-21 19:06:22 +0100

    better server

--- server.py
+++ server.py
@@ -51,9 +51,10 @@ def start_server():
         print(f"Connection from {addr}")
 
         with open('files/task4_dats_neurometric_07_05_2024.csv', newline='', encoding='utf-8') as csvfile:
-            csv_reader = csv.reader(csvfile)
+            csv_reader = csv.DictReader(csvfile,delimiter=';')
             for row in csv_reader:
-                message = ','.join(row)  # Convert row to a string
+                print(row.keys())
+                message = ','.join((row['workload'],row['stress'],row['workload_raw'],row['stress_raw']))  # Convert row to a string
                 conn.sendall(message.encode('utf-8'))  # Send as UTF-8
                 print(f"Sent: {message}")
                 time.sleep(0.2)  # Wait 0.2 seconds
